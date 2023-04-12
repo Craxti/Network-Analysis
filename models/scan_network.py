ae11a2d903576f2d763df50b9e9536320de7bc0a
Aleksandr Fetisov <alexgette17@gmail.com>
2023-04-12 13:45:33 +0400

    add scan

--- models/scan_network.py
+++ models/scan_network.py
@@ -1,5 +1,7 @@
 import subprocess
 import ipaddress
+import json
+import nmap
 
 
 def scan_ip_with_timeout(ip_address, timeout=2):
@@ -11,16 +13,33 @@ def scan_ip_with_timeout(ip_address, timeout=2):
         return ip_address, False
 
 
-def scan_network(cidr_address, timeout=2):
-    try:
-        network = ipaddress.IPv4Network(cidr_address)
-    except ValueError:
-        return f"{cidr_address} is not a valid CIDR network address"
+def scan_network(ip_range, output_format=None):
+    nm = nmap.PortScanner()
+    nm.scan(ip_range)
 
-    addresses = [str(ip_address) for ip_address in network.hosts()]
+    hosts = []
+    for host in nm.all_hosts():
+        host_data = {
+            "ip": host,
+            "hostname": nm[host].hostname() if "hostname" in nm[host] else "",
+            "state": nm[host].state(),
+            "ports": []
+        }
 
-    results = []
-    for address in addresses:
-        results.append(scan_ip_with_timeout(address, timeout))
+        if "tcp" in nm[host]:
+            for port in nm[host]["tcp"]:
+                port_data = {
+                    "port": port,
+                    "name": nm[host]["tcp"][port]["name"],
+                    "state": nm[host]["tcp"][port]["state"]
+                }
+                host_data["ports"].append(port_data)
 
-    return results
+        hosts.append(host_data)
+
+    # Output results
+    if output_format is None or output_format == "console":
+        print(json.dumps(hosts, indent=4))
+    elif output_format == "json":
+        with open("scan_results.json", "w") as f:
+            json.dump(hosts, f)
