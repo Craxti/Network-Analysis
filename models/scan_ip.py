1f35c47d7cb9b1d006c6e549dd238477d5bac273
Aleksandr Fetisov <55095407+Craxti@users.noreply.github.com>
2023-08-02 11:37:25 +0400

    Update ThreadPoolExecutor

--- models/scan_ip.py
+++ models/scan_ip.py
@@ -8,18 +8,27 @@ def scan_port(ip, port):
         sock.settimeout(2)
         result = sock.connect_ex((ip, port))
         if result == 0:
-            print(f"Port {port} on {ip} is open")
+            sock.close()
+            return port
         sock.close()
     except Exception as e:
         pass
+    return None
 
 
 def scan_ip(ip, start_port, end_port):
-    for port in range(start_port, end_port+1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(1)
-        result = sock.connect_ex((ip, port))
-        if result == 0:
-            return "IP is alive"
-        sock.close()
-    return "IP is dead"
+    open_ports = []
+    with ThreadPoolExecutor(max_workers=50) as executor:
+        # Формируем список задач для сканирования портов
+        tasks = [executor.submit(scan_port, ip, port) for port in range(start_port, end_port + 1)]
+
+        # Обрабатываем завершенные задачи и собираем результаты
+        for future in tasks:
+            result = future.result()
+            if result is not None:
+                open_ports.append(result)
+
+    if open_ports:
+        return f"IP {ip} is alive. Open ports: {open_ports}"
+    else:
+        return f"IP {ip} is dead."
